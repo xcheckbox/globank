86d607397b39cdda9813eac6899f3bd419ab4701
sergio-pascual <sergio.pascual@globant.com>
2023-02-23 00:27:10 -0600

    account card done

--- src/pages/DashboardPage/index.js
+++ src/pages/DashboardPage/index.js
@@ -1,11 +1,12 @@
 
+import { Accounts } from 'components/organisms/Accounts';
 import React from 'react';
 import styled from 'styled-components';
 
 export const Dashboard = () => {
   return (
     <div>
-      <h1>Contenido Principal</h1>
+      <Accounts />
     </div>
   )
 }
